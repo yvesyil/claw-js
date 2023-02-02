a43c0589d9d720c1f5b6d924018a24aed0af5414
yvesyil <doganyigityilmazz@gmail.com>
2023-02-02 17:37:10 +0000

    clock speed

--- src/opencl/kernel/matmul1.cl
+++ src/opencl/kernel/matmul1.cl
@@ -1,15 +1,15 @@
 // TODO implement type agnostic kernel
-__kernel void matmul1(__global const uint *lhs,
+__kernel void matmul1(__global const float *lhs,
   __global const size_t *lhs_dlen,
-  __global const uint *rhs,
+  __global const float *rhs,
   __global const size_t *rhs_dlen,
-  __global uint *res,
+  __global float *res,
   const size_t K /* col size of lhs, row size of rhs */ ) {
 
   const int row_index = get_global_id(0);
   const int col_index = get_global_id(1);
 
-  uint acc = 0;
+  float acc = 0.0f;
 
   for (size_t k = 0; k < K; k++) {
     acc += lhs[row_index + k * lhs_dlen[0]] * rhs[col_index * K + k];
--- tests/matmul_test.c
+++ tests/matmul_test.c
@@ -1,16 +1,19 @@
 #include "unity/unity.h"
 #include "../include/claw.h"
+#include "time.h"
 
 static struct claw_mat int8_mat;
 static struct claw_mat uint8_mat;
 static struct claw_mat int16_mat;
 static struct claw_mat uint16_mat;
 static struct claw_mat int32_mat;
-static struct claw_mat uint32_lhs;
-static struct claw_mat uint32_rhs;
+static struct claw_mat uint32_mat;
 static struct claw_mat int64_mat;
 static struct claw_mat uint64_mat;
-static struct claw_mat flt32_mat;
+
+static struct claw_mat flt32_lhs;
+static struct claw_mat flt32_rhs;
+
 static struct claw_mat flt64_mat;
 static struct claw_mat res;
 
@@ -21,13 +24,13 @@ void setUp()
 	claw_create_ones(&int16_mat, 10, 10, CLAW_INT16);
 	claw_create_ones(&uint16_mat, 10, 10, CLAW_UINT16);
 	claw_create_ones(&int32_mat, 10, 10, CLAW_INT32);
+	claw_create_ones(&uint32_mat, 10, 10, CLAW_UINT16);
 
-	claw_create_ones(&uint32_lhs, 32, 32, CLAW_UINT32);
-	claw_create_ones(&uint32_rhs, 32, 32, CLAW_UINT32);
+	claw_create_ones(&flt32_lhs, 1024, 1024, CLAW_FLT32);
+	claw_create_ones(&flt32_rhs, 1024, 1024, CLAW_FLT32);
 
 	claw_create_ones(&int64_mat, 10, 10, CLAW_INT64);
 	claw_create_ones(&uint64_mat, 10, 10, CLAW_UINT64);
-	claw_create_ones(&flt32_mat, 10, 10, CLAW_FLT32);
 	claw_create_ones(&flt64_mat, 10, 10, CLAW_FLT64);
 }
 
@@ -38,20 +41,25 @@ void tearDown()
 	claw_free(&int16_mat);
 	claw_free(&uint16_mat);
 	claw_free(&int32_mat);
+	claw_free(&uint32_mat);
 
-	claw_free(&uint32_lhs);
-	claw_free(&uint32_rhs);
+	claw_free(&flt32_lhs);
+	claw_free(&flt32_rhs);
 
 	claw_free(&int64_mat);
 	claw_free(&uint64_mat);
-	claw_free(&flt32_mat);
 	claw_free(&flt64_mat);
 }
 
 void matmul_test()
 {
-	claw_err err = claw_matmul(&uint32_lhs, &uint32_rhs, &res);
+	clock_t start = clock();
+	claw_err err = claw_matmul(&flt32_lhs, &flt32_rhs, &res);
+	clock_t end = clock();
 	TEST_ASSERT_TRUE(err == CLAW_SUCCESS);
+	claw_print_matrix(stdout, &res);
+	double duration = CLK_DUR_IN_MS(start, end);
+	printf("%f ms\n", duration);
 }
 
 int main()
