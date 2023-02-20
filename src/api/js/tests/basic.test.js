6cac175ff52609bcd56927d238b2f687411df69c
yvesyil <doganyigityilmazz@gmail.com>
2023-02-20 21:14:42 +0000

    a

--- src/api/js/tests/basic.test.js
+++ src/api/js/tests/basic.test.js
@@ -5,6 +5,7 @@ test('float matrix (32 x 32) matmul', () => {
     const b = new claw.Float32Mat(32, 32).fill(1);
 
     const c = claw.matmul(a, b);
+    c.print();
 });
 
 test('float identity matrix', () => {
